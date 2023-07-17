ec9775e0f22483750853a4e116b15ec72bf647c8
JhonCode117 <gizudajhovi@gmail.com>
2023-07-17 13:43:28 -0500

    feat: models

---
+++ server/src/models/Activity.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    //Model Definition  
+  sequelize.define('Activity', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    difficulty: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    duration: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    season: {
+      type: DataTypes.ENUM('Verano', 'Otoño', 'Invierno', 'Primavera'),
+      allowNull: false,
+    },
+  });
+};
--- server/src/models/Country.js
+++ server/src/models/Country.js
@@ -4,9 +4,42 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Country', {
+id: {
+      type: DataTypes.STRING(3),
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    flagImage: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subregion: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    area: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    population: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   });
 };
+
+
+
+    
