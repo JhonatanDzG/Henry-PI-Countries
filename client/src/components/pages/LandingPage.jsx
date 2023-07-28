8e4c77b6b427fd363132d522bbe61212024e00c6
JhonCode117 <gizudajhovi@gmail.com>
2023-07-27 21:33:46 -0500

    feat: landingPage/home linked

--- client/src/components/pages/LandingPage.jsx
+++ client/src/components/pages/LandingPage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "../css/landingPage.css"
+import { Link } from 'react-router-dom';
+
 
 export default function LandingPage() {
   return (
@@ -8,8 +10,44 @@ export default function LandingPage() {
         <h1>Discover the World in Data</h1>
       </div>
       <div className="button-container">
+        <Link to={"/home"}>
         <button className="button">Countries Information</button>
+        </Link>
       </div>
     </div>
   );
 }
+
+/*
+import React from 'react';
+import SearchBar from './SearchBar';
+import "./Css/Nav.css";
+import { Link } from 'react-router-dom';
+
+
+export default function Nav({onSearch, logOut}){
+    
+
+
+    return (
+
+        <div className='NavBar'>
+        <SearchBar onSearch = {onSearch}></SearchBar>
+        <div className='bAboutHome'>
+        <Link to={"/about"}>
+            <button>About</button>
+        </Link>
+        <Link to={"/home"}>
+            <button>Home</button>
+        </Link>
+      
+        
+        <button onClick={logOut}>LogOut</button>
+        
+       
+        </div>
+    </div>
+        )
+    
+    }
+*/
