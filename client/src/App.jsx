1b7d68f989194ea62357d816a65c255ab886a8b0
JhonCode117 <gizudajhovi@gmail.com>
2023-07-27 20:37:34 -0500

    style: landingPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return ( 
   <div className = 'App' >
         <Routes>
-        <Route path='landPage' element = {< LandingPage />} />
+        <Route path='/' element = {< LandingPage />} />
      </Routes>
   </div>
   )
--- client/src/components/pages/LandingPage.jsx
+++ client/src/components/pages/LandingPage.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import "../css/landingPage.css"
 
-export default function LandingPage () {
-    return(
-        <div>
-        <h1>Bienvenido a mi Landing Page</h1>
+export default function LandingPage() {
+  return (
+    <div className="landingPage-container">
+      <div className="title">
+        <h1>Discover the World in Data</h1>
       </div>
-    )
+      <div className="button-container">
+        <button className="button">Countries Information</button>
+      </div>
+    </div>
+  );
 }
