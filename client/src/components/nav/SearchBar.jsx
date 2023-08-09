2d1195ff2d965d6323d36c15578b32096602494e
JhonatanDzG <gizudajhovi@gmail.com>
2023-08-08 19:24:20 -0500

    feat: SearchBar.jsx

--- client/src/components/nav/SearchBar.jsx
+++ client/src/components/nav/SearchBar.jsx
@@ -1,13 +1,26 @@
 import "./searchBar.css";
 import { connect } from "react-redux";
 import { searchCountries } from "../../store/actions"
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 
  function SearchBar({search}) {
 
+  const location = useLocation();
+
+  const [searchValue, setSearchValue] = useState(""); // Estado para el valor del input
+
+
   const handleInputChange = async(event) => {
+    setSearchValue(event.target.value.trim())
    await search(event.target.value.trim());
   };
+
+
+  useEffect(() => {
+    setSearchValue("");
+  }, [location.pathname]);
 
   return (
     <div className="search-container">
@@ -16,6 +29,7 @@ import { searchCountries } from "../../store/actions"
         type="text"
         onChange={handleInputChange}
         placeholder="Try typing: col"
+        value={searchValue}
       />
     </div>
   );
